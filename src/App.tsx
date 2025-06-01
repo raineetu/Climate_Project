252a1976a70ca620327b5ae73734416de605e548
Raineetu <raineetu0070@gmail.com>
2025-06-01 18:21:18 +0545

    added openweather api and key in .env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import Layout from "./components/Layout"
 import { ThemeProvider } from "./components/context/Theme-provider"
 import WeatherDashboard from "./components/pages/WeatherDashboard"
@@ -19,6 +20,7 @@ function App() {
             </Layout>
           </ThemeProvider>
         </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
 
   )
