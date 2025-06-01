9c1b241f5fe633d1008fce0f54ce7459fba46f50
Raineetu <raineetu0070@gmail.com>
2025-06-01 08:52:58 +0545

    Wrapping the app with queryClientProvider so all components can use react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import Layout from "./components/Layout"
 import { ThemeProvider } from "./components/context/Theme-provider"
 import WeatherDashboard from "./components/pages/WeatherDashboard"
 import CityPages from "./components/pages/CityPages"
 
 function App() {
+  const queryClient = new QueryClient()
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark">
-        <Layout>
-          <Routes>
-          <Route path="/" element={<WeatherDashboard /> } />
-          <Route path="/city/:cityName" element={<CityPages /> } />
-          </Routes>
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeProvider defaultTheme="dark">
+            <Layout>
+              <Routes>
+              <Route path="/" element={<WeatherDashboard /> } />
+              <Route path="/city/:cityName" element={<CityPages /> } />
+              </Routes>
+            </Layout>
+          </ThemeProvider>
+        </BrowserRouter>
+    </QueryClientProvider>
+
   )
 }
 
